1f8ca2d477dd15b7a8753aba7a914ca08a6d0596
Taylor Smith <taylor.l.smith07@gmail.com>
2022-10-19 15:19:34 -0700

    5 queries added from compass

---
+++ 1_queries/4_most_visited_city.sql
@@ -0,0 +1,6 @@
+SELECT city, count(reservations.*) as total_reservations
+FROM properties
+JOIN reservations ON property_id = properties.id
+GROUP BY properties.city
+ORDER BY total_reservations DESC
+LIMIT 10;
